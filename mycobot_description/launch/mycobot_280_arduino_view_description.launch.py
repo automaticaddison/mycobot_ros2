01bc3568e9dcfe034844854fb15c8c20e6cd2514
Automatic Addison <automaticaddison@gmail.com>
2024-03-26 18:39:25 -0400

    Added launch file to visualize the URDF

---
+++ mycobot_description/launch/mycobot_280_arduino_view_description.launch.py
@@ -0,0 +1,105 @@
+# Author: Addison Sears-Collins
+# Date: March 26, 2024
+# Description: Display the robotic arm with RViz
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    # Define filenames    
+    urdf_package = 'mycobot_description'
+    urdf_filename = 'mycobot_280_urdf.xacro'
+    rviz_config_filename = 'mycobot_280_arduino_view_description.rviz'
+
+    # Set paths to important files
+    pkg_share_description = FindPackageShare(urdf_package)
+    default_urdf_model_path = PathJoinSubstitution([pkg_share_description, 'urdf', urdf_filename])
+    default_rviz_config_path = PathJoinSubstitution([pkg_share_description, 'rviz', rviz_config_filename])
+
+    # Launch configuration variables specific to simulation
+    jsp_gui = LaunchConfiguration('jsp_gui')
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
+    urdf_model = LaunchConfiguration('urdf_model')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Declare the launch arguments 
+    declare_jsp_gui_cmd = DeclareLaunchArgument(
+        name='jsp_gui', 
+        default_value='true', 
+        choices=['true', 'false'],
+        description='Flag to enable joint_state_publisher_gui')
+    
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        name='rviz_config_file',
+        default_value=default_rviz_config_path,
+        description='Full path to the RVIZ config file to use')
+
+    declare_urdf_model_path_cmd = DeclareLaunchArgument(
+        name='urdf_model', 
+        default_value=default_urdf_model_path, 
+        description='Absolute path to robot urdf file')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+    
+    # Specify the actions
+
+    # Publish the joint state values for the non-fixed joints in the URDF file.
+    start_joint_state_publisher_cmd = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        condition=UnlessCondition(jsp_gui))
+
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
+    start_joint_state_publisher_gui_cmd = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(jsp_gui))
+
+    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    robot_description_content = ParameterValue(Command(['xacro ', urdf_model]), value_type=str)
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time, 
+            'robot_description': robot_description_content}])
+      
+    # Launch RViz
+    start_rviz_cmd = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file],
+        parameters=[{
+        'use_sim_time': use_sim_time}])
+  
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_jsp_gui_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_urdf_model_path_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # Add any actions
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_joint_state_publisher_gui_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_rviz_cmd)
+
+    return ld
