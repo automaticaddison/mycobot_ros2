96b1262dc20d8b4991ef3eee42bebe067a471604
Automatic Addison <automaticaddison@gmail.com>
2024-04-28 07:57:11 -0400

    Remove the need for prior conversion from xacro to URDF

--- mycobot_gazebo/launch/mycobot_280_arduino_bringup_classic_gazebo.launch.py
+++ mycobot_gazebo/launch/mycobot_280_arduino_bringup_classic_gazebo.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
   gazebo_launch_file_path = 'launch'
   gazebo_models_path = 'models'
   rviz_config_file_path = 'rviz/mycobot_280_arduino_view_description.rviz'
-  urdf_file_path = 'urdf/mycobot_280_classic_gazebo.urdf'
+  urdf_file_path = 'urdf/mycobot_280_classic_gazebo.urdf.xacro'
   world_file_path = 'worlds/empty_classic.world' # Example: 'worlds/house_classic.world', 'worlds/empty_classic.world'
 
   # Set the path to different files and folders.  
@@ -176,7 +176,7 @@ def generate_launch_description():
     executable='spawn_entity.py',
     arguments=[
       '-entity', robot_name,
-      '-file', urdf_model, 
+      '-topic', "robot_description",  
       '-x', x,
       '-y', y,
       '-z', z,
