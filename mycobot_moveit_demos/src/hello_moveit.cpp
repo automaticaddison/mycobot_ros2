e06022d59a7717ac06cededfdaa5b92ae3114527
Automatic Addison <automaticaddison@gmail.com>
2024-12-15 08:11:51 -0500

    Added moveit demos

---
+++ mycobot_moveit_demos/src/hello_moveit.cpp
@@ -0,0 +1,112 @@
+/**
+ * @file hello_moveit.cpp
+ * @brief A basic ROS 2 and MoveIt 2 program to control a robot arm
+ *
+ * This program demonstrates how to use ROS 2 and MoveIt 2 to control a robot arm.
+ * It sets up a node, creates a MoveGroupInterface for the arm, sets a target pose for the gripper_base,
+ * plans a trajectory, and executes the planned motion.
+ *
+ * @author Addison Sears-Collins
+ * @date December 15, 2024
+ */
+
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+/**
+ * @brief The main function that starts our program.
+ *
+ * This function sets up our ROS 2 environment and prepares it for robot control.
+ *
+ * @param argc The number of input arguments our program receives.
+ * @param argv The list of input arguments our program receives.
+ * @return int A number indicating if our program finished successfully (0) or not.
+ */
+int main(int argc, char * argv[])
+{
+  // Start up ROS 2
+  rclcpp::init(argc, argv);
+
+  // Creates a node named "hello_moveit". The node is set up to automatically
+  // handle any settings (parameters) we might want to change later without editing the code.
+  auto const node = std::make_shared<rclcpp::Node>(
+    "hello_moveit",
+    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+  );
+
+  // Creates a "logger" that we can use to print out information or error messages
+  // as our program runs.
+  auto const logger = rclcpp::get_logger("hello_moveit");
+
+  // Create the MoveIt MoveGroup Interfaces
+  // These interfaces are used to plan and execute movements, set target poses,
+  // and perform other motion-related tasks for each respective part of the robot.
+  // The use of auto allows the compiler to automatically deduce the type of variable.
+  // Source: https://github.com/moveit/moveit2/blob/main/moveit_ros/planning_interface/move_group_interface/include/moveit/move_group_interface/move_group_interface.h
+  using moveit::planning_interface::MoveGroupInterface;
+  auto arm_group_interface = MoveGroupInterface(node, "arm");
+
+  // Specify a planning pipeline to be used for further planning
+  arm_group_interface.setPlanningPipelineId("ompl");
+
+  // Specify a planner to be used for further planning
+  arm_group_interface.setPlannerId("RRTConnectkConfigDefault");
+
+  // Specify the maximum amount of time in seconds to use when planning
+  arm_group_interface.setPlanningTime(1.0);
+
+  // Set a scaling factor for optionally reducing the maximum joint velocity. Allowed values are in (0,1].
+  arm_group_interface.setMaxVelocityScalingFactor(1.0);
+
+  //  Set a scaling factor for optionally reducing the maximum joint acceleration. Allowed values are in (0,1].
+  arm_group_interface.setMaxAccelerationScalingFactor(1.0);
+
+  // Display helpful logging messages on the terminal
+  RCLCPP_INFO(logger, "Planning pipeline: %s", arm_group_interface.getPlanningPipelineId().c_str());
+  RCLCPP_INFO(logger, "Planner ID: %s", arm_group_interface.getPlannerId().c_str());
+  RCLCPP_INFO(logger, "Planning time: %.2f", arm_group_interface.getPlanningTime());
+
+  // Set a target pose for the end effector of the arm
+  auto const arm_target_pose = [&node]{
+    geometry_msgs::msg::PoseStamped msg;
+    msg.header.frame_id = "base_link";
+    msg.header.stamp = node->now();
+    msg.pose.position.x = 0.061;
+    msg.pose.position.y = -0.176;
+    msg.pose.position.z = 0.168;
+    msg.pose.orientation.x = 1.0;
+    msg.pose.orientation.y = 0.0;
+    msg.pose.orientation.z = 0.0;
+    msg.pose.orientation.w = 0.0;
+    return msg;
+  }();
+  arm_group_interface.setPoseTarget(arm_target_pose);
+
+  // Create a plan to that target pose
+  // This will give us two things:
+  // 1. Whether the planning was successful (stored in 'success')
+  // 2. The actual motion plan (stored in 'plan')
+  auto const [success, plan] = [&arm_group_interface] {
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(arm_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  // Try to execute the movement plan if it was created successfully
+  // If the plan wasn't successful, report an error
+  // Execute the plan
+  if (success)
+  {
+    arm_group_interface.execute(plan);
+  }
+    else
+  {
+    RCLCPP_ERROR(logger, "Planning failed!");
+  }
+
+  // Shut down ROS 2 cleanly when we're done
+  rclcpp::shutdown();
+  return 0;
+}
