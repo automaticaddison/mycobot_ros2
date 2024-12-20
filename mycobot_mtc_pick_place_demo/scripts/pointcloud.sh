54a371301be5eb312be68fa5f01b38bf9f9797ca
Automatic Addison <automaticaddison@gmail.com>
2024-12-20 10:13:54 -0500

    Add scripts folder

---
+++ mycobot_mtc_pick_place_demo/scripts/pointcloud.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Single script to launch point cloud viewing
+
+cleanup() {
+    echo "Cleaning up..."
+    sleep 5.0
+    pkill -9 -f "ros2|gazebo|gz|nav2|amcl|bt_navigator|nav_to_pose|rviz2|assisted_teleop|cmd_vel_relay|robot_state_publisher|joint_state_publisher|move_to_free|mqtt|autodock|cliff_detection|moveit|move_group|basic_navigator"
+}
+
+
+# Set up cleanup trap
+trap 'cleanup' SIGINT SIGTERM
+
+echo "Launching Gazebo simulation..."
+ros2 launch mycobot_gazebo mycobot.gazebo.launch.py \
+    load_controllers:=true \
+    world_file:=pick_and_place_demo.world \
+    use_camera:=true \
+    use_rviz:=false \
+    use_robot_state_pub:=true \
+    use_sim_time:=true \
+    x:=0.0 \
+    y:=0.0 \
+    z:=0.05 \
+    roll:=0.0 \
+    pitch:=0.0 \
+    yaw:=0.0 &
+
+sleep 15
+echo "Adjusting camera position..."
+gz service -s /gui/move_to/pose \
+    --reqtype gz.msgs.GUICamera \
+    --reptype gz.msgs.Boolean \
+    --timeout 2000 \
+    --req "pose: {position: {x: 1.36, y: -0.58, z: 0.95} orientation: {x: -0.26, y: 0.1, z: 0.89, w: 0.35}}" &
+
+sleep 10
+
+#ros2 launch mycobot_mtc_pick_place_demo point_cloud_viewer.launch.py file_name:=/home/ubuntu/Downloads/4_convertToPCL_debug_cloud.pcd
+#ros2 launch mycobot_mtc_pick_place_demo point_cloud_viewer.launch.py file_name:=/home/ubuntu/Downloads/5_support_plane_debug_cloud.pcd
+ros2 launch mycobot_mtc_pick_place_demo point_cloud_viewer.launch.py file_name:=/home/ubuntu/Downloads/5_objects_cloud_debug_cloud.pcd
+
+# Keep the script running until Ctrl+C
+wait
---
+++ mycobot_mtc_pick_place_demo/scripts/robot.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Single script to launch the mycobot with Gazebo, RViz, and MoveIt 2
+
+cleanup() {
+    echo "Cleaning up..."
+    sleep 5.0
+    pkill -9 -f "ros2|gazebo|gz|nav2|amcl|bt_navigator|nav_to_pose|rviz2|assisted_teleop|cmd_vel_relay|robot_state_publisher|joint_state_publisher|move_to_free|mqtt|autodock|cliff_detection|moveit|move_group|basic_navigator"
+}
+
+# Set up cleanup trap
+trap 'cleanup' SIGINT SIGTERM
+
+echo "Launching Gazebo simulation..."
+ros2 launch mycobot_gazebo mycobot.gazebo.launch.py \
+    load_controllers:=true \
+    world_file:=pick_and_place_demo.world \
+    use_camera:=true \
+    use_rviz:=false \
+    use_robot_state_pub:=true \
+    use_sim_time:=true \
+    x:=0.0 \
+    y:=0.0 \
+    z:=0.05 \
+    roll:=0.0 \
+    pitch:=0.0 \
+    yaw:=0.0 &
+
+sleep 15
+echo "Launching the move group interface..."
+ros2 launch mycobot_moveit_config move_group.launch.py \
+    rviz_config_file:=mtc_demos.rviz \
+    rviz_config_package:=mycobot_mtc_demos &
+
+echo "Adjusting camera position..."
+gz service -s /gui/move_to/pose \
+    --reqtype gz.msgs.GUICamera \
+    --reptype gz.msgs.Boolean \
+    --timeout 2000 \
+    --req "pose: {position: {x: 1.36, y: -0.58, z: 0.95} orientation: {x: -0.26, y: 0.1, z: 0.89, w: 0.35}}" &
+
+sleep 10
+echo "Launching the Pick and Place demo..."
+ros2 launch mycobot_mtc_pick_place_demo get_planning_scene_server.launch.py &
+sleep 5
+ros2 launch mycobot_mtc_pick_place_demo pick_place_demo.launch.py
+
+# Keep the script running until Ctrl+C
+wait
+
+
+
+
+
