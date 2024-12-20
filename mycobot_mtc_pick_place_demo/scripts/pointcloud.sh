a350c883f14d42fce52281d4e44abb683590e4dd
Automatic Addison <automaticaddison@gmail.com>
2024-12-20 18:51:49 -0500

    Successful pick and place

--- mycobot_mtc_pick_place_demo/scripts/pointcloud.sh
+++ mycobot_mtc_pick_place_demo/scripts/pointcloud.sh
@@ -21,7 +21,7 @@ ros2 launch mycobot_gazebo mycobot.gazebo.launch.py \
     use_sim_time:=true \
     x:=0.0 \
     y:=0.0 \
-    z:=0.05 \
+    z:=0.0 \
     roll:=0.0 \
     pitch:=0.0 \
     yaw:=0.0 &
